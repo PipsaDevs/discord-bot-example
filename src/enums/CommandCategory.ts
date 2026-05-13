4bb3a613458ecc4d125fe321fad826e99c140a16
CyRstudent <hsmad2022@gmail.com>
2026-05-13 11:59:20 +0200

    feat: Add Command categories in enum format

---
+++ src/enums/CommandCategory.ts
@@ -0,0 +1,11 @@
+/**
+ * This enum defines the categories a command can fall within
+ */
+
+enum CommandCategory {
+	DEV = 'development',
+	MOD = 'moderation',
+	// Will add more as more commands are written
+}
+
+export default CommandCategory;
