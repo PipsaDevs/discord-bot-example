bb220bc186337d317080284cdd5f90d6dac47b2f
CyRstudent <hsmad2022@gmail.com>
2026-05-14 14:03:02 +0200

    feat: Add ping Slash Command

---
+++ src/slash_commands/ping.ts
@@ -0,0 +1,36 @@
+import type { SlashCommand } from '../interfaces/SlashCommand.js';
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { scape } from '../util/scape.js';
+import CommandCategory from '../enums/CommandCategory.js';
+const pCmd: SlashCommand = {
+	data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
+	category: CommandCategory.INFO,
+	async execute(interaction, client) {
+		const sum = Date.now() - interaction.createdTimestamp;
+		let wsStr: string = client.ws.ping.toString();
+		if (client.ws.ping > 0) {
+			wsStr = `+${wsStr}`;
+		}
+		const embed = new EmbedBuilder()
+			.setColor('Blurple')
+			.setTitle('Pong')
+			.addFields(
+				{
+					name: 'Websocket',
+					value: scape('diff', `${wsStr} ms`),
+					inline: true,
+				},
+				{
+					name: 'Bot',
+					value: scape('diff', `+${sum} ms`),
+					inline: true,
+				},
+			)
+			.setTimestamp(new Date());
+		await interaction.reply({
+			embeds: [embed],
+		});
+	},
+};
+
+export default pCmd;
